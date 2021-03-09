7554b1761394df69ba4c12fcc18d22767960cc44
usman-naeem728 <76055167+usman-naeem728@users.noreply.github.com>
2021-03-08 22:13:56 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,106 @@
+function login(){
+    var emai = document.getElementsByName("email")
+    var pass = document.getElementsByName("psw")
+    console.log(emai.value,pass.value)
+
+}
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Content Writer", "Blogger", "Creative Writer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Content Writer", "Blogger", "Creative Writer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+// function openForm() {
+//     document.getElementById("myForm").style.display = "block";
+//   }
+  
+//   function closeForm() {
+//     document.getElementById("myForm").style.display = "none";
+//   }
+
+function loginan(){
+
+    var name = document.getElementById("name")
+    var email = document.getElementById("email")
+    var msg = document.getElementById("msg")
+if(name.value === "" || email.value === "" || msg.value === ""){
+    alert("Please give all Information")
+}else{
+    var contact = {
+        email: email.value,
+        name: name.value,
+        msg: msg.value
+    }
+
+firebase.database().ref("/contact/").push(contact)
+
+
+alert("Thank you for contacting me!I will get back to you soon")
+}
+}
